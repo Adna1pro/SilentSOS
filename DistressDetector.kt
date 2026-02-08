4ce7ee846758f3009db173e2da4f32a2c1c13181
adna1pro <adithiyanatarajan@gmail.com>
2026-02-08 07:29:33 +0530

    sos management to send message and call

--- DistressDetector.kt
+++
@@ -1,9 +0,0 @@
-package com.example.silentsos
-
-class DistressDetector {
-    fun isDistress(acceleration: Float): Boolean {
-        // For now, let's assume a simple threshold.
-        // We can refine this later.
-        return acceleration > 20.0f
-    }
-}
